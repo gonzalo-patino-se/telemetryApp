3928e15f3e15eb419e128621cc2d0a27b25aea81
Gonzalo <gonzalo.patino@se.com>
2025-11-04 14:49:04 -0800

    Initial commit, clean history

---
+++ telemetryapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TelemetryViewSet, adx_telemetry
+
+router = DefaultRouter()
+router.register(r'telemetry', TelemetryViewSet, basename='telemetry')
+
+urlpatterns = [
+   
+    path('', include(router.urls)),  # Include the router URLs
+    path('adx/', adx_telemetry), # Endpoint for ADX telemetry query
+]
