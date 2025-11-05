c09f552bef177426afea60323fd61bfbe8e43545
Gonzalo Patino <gonzalo.patino@se.com>
2025-11-05 12:27:03 -0800

    Improved styling on admin page

--- telemetryapp/views.py
+++ telemetryapp/views.py
@@ -3,11 +3,16 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .adx_service import query_adx
 
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
 from rest_framework import viewsets
 from .models import Telemetry
 from .serializers import TelemetrySerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def adx_telemetry(request):
     kql_query = "DevInfo | limit 2"  # Example KQL query
     data = query_adx(kql_query)
