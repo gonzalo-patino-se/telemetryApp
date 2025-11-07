e70b808ada979d4a3d9a48ae4b99226c30de6198
Gonzalo Patino <gonzalo.patino@se.com>
2025-11-06 19:32:25 -0800

    JWT working, Security Working, API endpoints with KQL query Serial Number works

--- telemetryapp/urls.py
+++ telemetryapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TelemetryViewSet, adx_telemetry
+from .views import TelemetryViewSet, adx_telemetry, search_serial
 
 router = DefaultRouter()
 router.register(r'telemetry', TelemetryViewSet, basename='telemetry')
@@ -9,5 +9,6 @@ router.register(r'telemetry', TelemetryViewSet, basename='telemetry')
 urlpatterns = [
     path('', include(router.urls)),  # API routes for TelemetryViewSet
     path('adx/', adx_telemetry),     # Endpoint for ADX telemetry query
+    path('search_serial/', search_serial), # Endpoint for serial number search
 ]
 
